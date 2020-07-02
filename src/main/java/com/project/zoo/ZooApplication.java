50f82e29212bf422243180089d4ba830adc67fc4
jebrildguev <jebrilguev@gmail.com>
2020-07-02 14:44:29 -0400

    Adds code to run Spring Boot app

---
+++ src/main/java/com/project/zoo/ZooApplication.java
@@ -0,0 +1,17 @@
+package com.project.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooApplication.class, args);
+	}
+
+}
